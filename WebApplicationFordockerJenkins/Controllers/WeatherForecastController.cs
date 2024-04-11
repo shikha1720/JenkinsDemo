33e7fb30c915103d40b41de3cb76f7948f64b8bf
Shikha Jain <117159179+shikha1720@users.noreply.github.com>
2024-04-11 12:09:09 +0530

    Update WeatherForecastController.cs

--- WebApplicationFordockerJenkins/Controllers/WeatherForecastController.cs
+++ WebApplicationFordockerJenkins/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplicationFordockerJenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "VeryCold"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
